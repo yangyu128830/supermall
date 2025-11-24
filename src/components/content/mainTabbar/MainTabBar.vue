d0d4bc27059e7ffdc70faf09b9243caae7516fde
ysy13 <ysy13044878810@qq.com>
2025-11-24 12:14:35 +0800

    1

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -20,6 +20,11 @@
       <img slot="active-icon" src="~assets/img/tabbar/profile_active.svg" alt="">
       <div slot="text">我的</div>
     </tab-bar-item>
+    <tab-bar-item link="/member">
+      <img slot="icon" src="~assets/img/tabbar/profile.svg" alt="">
+      <img slot="active-icon" src="~assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="text">会员中心</div>
+    </tab-bar-item>
   </tab-bar>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ const Category = () => import('views/category/Category')
 const Cart = () => import('views/cart/Cart')
 const Profile = () => import('views/profile/Profile')
 const Detail = () => import('views/detail/Detail')
+const Member = () => import('views/member/Member')
 
 // 1.安装VueRouter
 Vue.use(VueRouter)
@@ -31,6 +32,10 @@ const routes = [
   {
     path: '/profile',
     component: Profile
+  },
+  {
+    path: '/member',
+    component: Member
   },
   {
     path: '/detail',
